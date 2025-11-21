5c5a2f1a6924ef0e7f32b74aaf417637c94dffed
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-11-20 16:45:04 -0800

    feat(wip): placeholders

--- lib/core/cache.dart
+++ lib/core/cache.dart
@@ -1,4 +1,5 @@
 import '../config/config.dart';
+import 'ctx.dart';
 
 class PVCache {
   static final Map<String, PVCache> _instances = {};
@@ -17,4 +18,65 @@ class PVCache {
 
     return PVCache._(config);
   }
+
+  // cache functions
+  Future<dynamic> get(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.get);
+    return runtime.getResult();
+  }
+
+  Future<void> put(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.put);
+  }
+
+  Future<void> delete(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.delete);
+  }
+
+  Future<void> clear(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.clear);
+  }
+
+  Future<Iterable<dynamic>> iterateKey(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.iterateKey);
+    return runtime.getResult();
+  }
+
+  Future<Iterable<dynamic>> iterateValue(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.iterateValue);
+    return runtime.getResult();
+  }
+
+  Future<Iterable<MapEntry<dynamic, dynamic>>> iterateEntry(PVCtx ctx) async {
+    final runtime = PVRuntimeCtx.fromConfig(_config, ctx);
+    await runtime.invokeStage(_config.sequenceConfig.iterateEntry);
+    return runtime.getResult();
+  }
+
+  Future<void> dispose() async {
+    _instances.remove(_config.env);
+  }
+
+  // helpers
+  /// ifNotCached
+  Future<dynamic> ifNotCached(
+      PVCtx ctx,
+      Future<dynamic> Function() computeFunction,
+      ) async {
+    var cachedValue = await get(ctx);
+    if (cachedValue != null) {
+      return cachedValue;
+    }
+
+    var computedValue = await computeFunction();
+    await put(ctx);
+    return computedValue;
+  }
+
 }
--- lib/core/ctx.dart
+++ lib/core/ctx.dart
@@ -1,10 +1,54 @@
-
+import '../config/config.dart';
+import '../config/hook.dart';
+import '../config/registry.dart';
 
 class PVCtx {
-  
+  final String key;
+  final dynamic value;
+  final Map<String, dynamic> metadata;
+  final ImmutablePVCHookRegistry? hookRegistry;
+
+  PVCtx({
+    required this.key,
+    this.value,
+    Map<String, dynamic>? metadata,
+    this.hookRegistry,
+  }) : metadata = Map.unmodifiable(metadata ?? {});
 }
 
+class PVRuntimeCtx {
+  final PVCtx initialCtx;
+  final PVConfig config;
 
-class PVRuntimeCtx extends PVCtx {
+  final Map<String, dynamic> runtimeMap = {};
+  final Map<String, dynamic> metadataMap = {};
+  final Map<String, dynamic> tempMap = {};
+
+  PVRuntimeCtx._({
+    required this.initialCtx,
+    required this.config,
+  });
+
+  factory PVRuntimeCtx.fromConfig(PVConfig config, PVCtx initialCtx) {
+    return PVRuntimeCtx._(
+      initialCtx: initialCtx,
+      config: config,
+    );
+  }
+
+  Future<void> invokeStage(List<String> stages) async {
+    final stageHooks = stages
+        .where((stage) => config.hookRegistry.stageHooks.containsKey(stage))
+        .map((stage) => config.hookRegistry.stageHooks[stage]!)
+        .toList();
+
+    for (var hook in stageHooks) {
+      await hook.hookFunction(this);
+    }
+  }
+
+  dynamic getResult() {
+    return runtimeMap['result'];
+  }
   
 }
